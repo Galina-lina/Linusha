527a9433cb7e57a130964afceeb693e23b1691ca
Galina Ledyaeva <58899604+Galina-lina@users.noreply.github.com>
2022-01-10 12:39:33 +0300

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.product (name, brand, price, quantity) VALUES ('Harry Potter', 'Rosmen', 100, 2);
+INSERT INTO public.product (name, brand, price, quantity) VALUES ('Home alone', 'AST', 120,50);
+INSERT INTO public.product (name, brand, price, quantity) VALUES ('The Little Prince', 'AST', 55, 20);
+INSERT INTO public.product (name, brand, price, quantity) VALUES ('Notre-Dame de Paris', 'world books',289, 56);

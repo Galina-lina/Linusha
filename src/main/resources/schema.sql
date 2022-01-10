6e4c2c1130919ed749ea18b9d675e6e0df312d83
Galina Ledyaeva <58899604+Galina-lina@users.noreply.github.com>
2022-01-10 15:19:14 +0300

    Add files via upload

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,4 +12,7 @@ CREATE TABLE product
         quantity INTEGER         NOT NULL
 );
 
-
+INSERT INTO public.product (name, brand, price, quantity) VALUES ('Harry Potter', 'Rosmen', 100, 2);
+INSERT INTO public.product (name, brand, price, quantity) VALUES ('Home alone', 'AST', 120,50);
+INSERT INTO public.product (name, brand, price, quantity) VALUES ('The Little Prince', 'AST', 55, 20);
+INSERT INTO public.product (name, brand, price, quantity) VALUES ('Notre-Dame de Paris', 'world books',289, 56);
